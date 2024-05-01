cb35c495eda01fb6db229b8953a2654c11a87886
백원재 <81965519+Cinema-Paradiso@users.noreply.github.com>
2024-05-02 02:58:32 +0900

    24.05.02(success)
    메일 보내기 성공
    구글로 보내면 성공하는데 네이버 메일 규격으로는 실패
    
    비밀번호 보안 해야함

--- src/main/java/CapstoneDesign/EvacuationGuide/controller/EmailController.java
+++ src/main/java/CapstoneDesign/EvacuationGuide/controller/EmailController.java
@@ -23,6 +23,6 @@ public class EmailController {
     @GetMapping("/sendEmail")
     public String sendEmail(@RequestParam String to) {
         emailService.sendEmail(to, "Test Email", "This is a test email from Spring Boot application.");
-        return "Email sent successfully!!";
+        return "mail/defalut";
     }
 }
