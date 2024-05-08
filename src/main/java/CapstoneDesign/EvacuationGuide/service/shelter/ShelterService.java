18f5ce517530867f2f467ced439d6ddd2f0580e1
백원재 <81965519+Cinema-Paradiso@users.noreply.github.com>
2024-05-08 14:38:24 +0900

    24.05.08
    Map<Shelter, Double>로 각 대피소 별 거리까지 계산
    Haversine 따로 클래스 만들었음

--- src/main/java/CapstoneDesign/EvacuationGuide/service/shelter/ShelterService.java
+++ src/main/java/CapstoneDesign/EvacuationGuide/service/shelter/ShelterService.java
@@ -1,8 +1,11 @@
 package CapstoneDesign.EvacuationGuide.service.shelter;
 
-//import aj.org.objectweb.asm.TypeReference;
+//import aj.org.objectweb.asm.TypeReference; 해당 import와
+//import com.fasterxml.jackson.core.type.TypeReference;가 충돌
+
 import CapstoneDesign.EvacuationGuide.domain.Shelter;
 import CapstoneDesign.EvacuationGuide.repository.ShelterRepository;
+import CapstoneDesign.EvacuationGuide.usefulMethod.Haversine;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +14,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ShelterService {
@@ -26,7 +31,8 @@ public class ShelterService {
     public void saveJson() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         ClassPathResource resource = new ClassPathResource("json/seoul.json");
-        List<Shelter> shelters = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<Shelter>>() {});
+        List<Shelter> shelters = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<Shelter>>() {
+        });
 
         for (Shelter shelter : shelters) {
             shelterRepository.save(shelter);
@@ -43,23 +49,25 @@ public class ShelterService {
         }
     }
 
-    public void findShelters(){
-        Shelter shortestShelter=null;
-        Double shortestDistance=5.0;
+    public void findShelters() {
+        Shelter shortestShelter = null;
+        Double shortestDistance = 5.0;
+        Map<Shelter, Double> findshelterMap = new HashMap<Shelter, Double>();
+        Haversine haversine = new Haversine();
 
-
-        List<Shelter> findShelters=new ArrayList<>();
+        List<Shelter> findShelters = new ArrayList<>();
         List<Shelter> shelters = shelterRepository.findAll();
         for (Shelter shelter : shelters) {
-             double distance = distanceInKilometerByHaversine(shelter.getLatitude(),
+            double distance = haversine.distanceInKilometerByHaversine(shelter.getLatitude(),
                     shelter.getLongitude(),
                     37.579617
                     , 126.977041);
-            if (distance < 5){   //5km
+            if (distance < 5) {   //5km
                 findShelters.add(shelter);
+                findshelterMap.put(shelter, distance);
                 if (distance < shortestDistance) {
-                    shortestShelter=shelter;
-                    shortestDistance=distance;
+                    shortestShelter = shelter;
+                    shortestDistance = distance;
                 }
             }
         }
@@ -72,35 +80,36 @@ public class ShelterService {
             return;
         }
         System.out.printf("해당 대피소까지의 직선거리는 약 ");
-        System.out.printf("%.2f",shortestDistance); //소수점 2자리까지만 출력
+        System.out.printf("%.2f", shortestDistance); //소수점 2자리까지만 출력
         System.out.println("km 입니다.");
         System.out.println("=================================================================");
         System.out.println("현재 위치로부터 직선거리 5km 내 위치한 대피소는 총 " + findShelters.size() + "개입니다.");
         System.out.println("리스트는 아래와 같습니다.");
         for (Shelter findShelter : findShelters) {
-            System.out.println("대피소 이름 = " + findShelter.getName());
-            //System.out.println("직선거리 = " + findShelter.getName());
+            System.out.printf("[" + findShelter.getName());
+            System.out.println(", " + String.format("%.2f", findshelterMap.get(findShelter)) + "km]");
         }
     }
-
-    //두 지점 사이의 직선 거리 구하는 공식
-    //두 지점 각각의 위도, 경도 필요
-    public static double distanceInKilometerByHaversine(double latitude1, double longitude1, double latitude2, double longitude2) {
-        double distance;
-        double radius = 6371; // 지구 반지름(km)
-        double toRadian = Math.PI / 180;
-
-        double deltaLatitude = Math.abs(latitude1 - latitude2) * toRadian;
-        double deltaLongitude = Math.abs(longitude1 - longitude2) * toRadian;
-
-        double sinDeltaLat = Math.sin(deltaLatitude / 2);
-        double sinDeltaLng = Math.sin(deltaLongitude / 2);
-        double squareRoot = Math.sqrt(
-                sinDeltaLat * sinDeltaLat +
-                        Math.cos(latitude1 * toRadian) * Math.cos(latitude2 * toRadian) * sinDeltaLng * sinDeltaLng);
-
-        distance = 2 * radius * Math.asin(squareRoot);
-
-        return distance;
-    }
 }
+
+//    //두 지점 사이의 직선 거리 구하는 공식
+//    //두 지점 각각의 위도, 경도 필요
+//    public static double distanceInKilometerByHaversine(double latitude1, double longitude1, double latitude2, double longitude2) {
+//        double distance;
+//        double radius = 6371; // 지구 반지름(km)
+//        double toRadian = Math.PI / 180;
+//
+//        double deltaLatitude = Math.abs(latitude1 - latitude2) * toRadian;
+//        double deltaLongitude = Math.abs(longitude1 - longitude2) * toRadian;
+//
+//        double sinDeltaLat = Math.sin(deltaLatitude / 2);
+//        double sinDeltaLng = Math.sin(deltaLongitude / 2);
+//        double squareRoot = Math.sqrt(
+//                sinDeltaLat * sinDeltaLat +
+//                        Math.cos(latitude1 * toRadian) * Math.cos(latitude2 * toRadian) * sinDeltaLng * sinDeltaLng);
+//
+//        distance = 2 * radius * Math.asin(squareRoot);
+//
+//        return distance;
+//    }
+
--- src/main/java/CapstoneDesign/EvacuationGuide/usefulMethod/Haversine.java
+++ src/main/java/CapstoneDesign/EvacuationGuide/usefulMethod/Haversine.java
@@ -1,23 +1,23 @@
-/*
 package CapstoneDesign.EvacuationGuide.usefulMethod;
 
-public class Haversine() {
-
-
+public class Haversine {
     double distance;
     double radius = 6371; // 지구 반지름(km)
     double toRadian = Math.PI / 180;
 
-    double deltaLatitude = Math.abs(x1 - x2) * toRadian;
-    double deltaLongitude = Math.abs(y1 - y2) * toRadian;
+    public double distanceInKilometerByHaversine(double latitude1, double longitude1, double latitude2, double longitude2) {
+        double deltaLatitude = Math.abs(latitude1 - latitude2) * toRadian;
+        double deltaLongitude = Math.abs(longitude1 - longitude2) * toRadian;
 
-    double sinDeltaLat = Math.sin(deltaLatitude / 2);
-    double sinDeltaLng = Math.sin(deltaLongitude / 2);
-    double squareRoot = Math.sqrt(
-            sinDeltaLat * sinDeltaLat +
-                    Math.cos(x1 * toRadian) * Math.cos(x2 * toRadian) * sinDeltaLng * sinDeltaLng);
+        double sinDeltaLat = Math.sin(deltaLatitude / 2);
+        double sinDeltaLng = Math.sin(deltaLongitude / 2);
+        double squareRoot = Math.sqrt(
+                sinDeltaLat * sinDeltaLat +
+                        Math.cos(latitude1 * toRadian) * Math.cos(latitude2 * toRadian) * sinDeltaLng * sinDeltaLng);
 
-    distance = 2 * radius * Math.asin(squareRoot);
+        distance = 2 * radius * Math.asin(squareRoot);
 
-    return distance;
-}*/
+        return distance;
+    }
+
+}
